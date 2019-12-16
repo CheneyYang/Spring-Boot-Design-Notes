a63010060c26d76201bc4437d8aea70c3c893034
Yanggc <younggc@foxmail.com>
2019-12-17 07:07:14 +0800

    使用self4j作为日志组件，并且添加自定义配置

--- src/main/java/com/github/example/controller/User.java
+++ src/main/java/com/github/example/controller/User.java
@@ -1,8 +1,8 @@
 package com.github.example.controller;
 
 import com.github.example.config.UserProperties;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class User {
-    private static final Logger logger = LogManager.getLogger(User.class);
-
+//    private static final Logger logger = LogManager.getLogger(User.class);
+    private static Logger logger = LoggerFactory.getLogger(User.class);
     @Autowired
     UserProperties userConfig;
 
